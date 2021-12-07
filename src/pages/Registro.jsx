d46c06a7312e65c0e732bc03a3caceaf4427f6c3
leiv03 <nadia.guarisco@anima.edu.uy>
2021-12-07 11:33:36 -0300

    ok

--- src/pages/Registro.jsx
+++ src/pages/Registro.jsx
@@ -1,11 +1,24 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
 
 const Registro = () => (
   <div>
-      <form action="">
-          <input type="text" />
-      </form>
+    <p>hola mundi</p>
   </div>
+  /*/<div>
+    const Perfil = (props) => {
+      const [nombre, setNombre] =useState('pepito');
+      const manejarSubmit = () => {
+
+      };
+    }
+    <form action="">
+      <label>
+        Nombre:
+        <input type="text" placeholder="nombre" required value={npmbre} onChange={(e) -> setNombre (e.   )} />
+      </label>
+    </form>
+  </div>*/
 );
 
 export default Registro;
